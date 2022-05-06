7c3660ce7e645cccf1826f50a7e55a2caed0351a
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-05-06 22:53:49 +0530

    Time: 23 ms (72.43%), Space: 10.8 MB (49.54%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,40 +1,50 @@
 class Solution {
 public:
-    string removeDuplicates(string s, int k) {
-        
-        int n = s.length();
-        vector<int> continousFreqCount(n, 1);
-        stack<int> st;
-        string ans;
-        
-        st.push(0);
-        
-        for(int i = 1; i < n ; i++){
-            
-            if(!st.empty()){
-                if(s[st.top()]==s[i]) continousFreqCount[i] = continousFreqCount[st.top()] + 1;
+   string removeDuplicates(string s, int k) {
+        stack<pair<char,int>>st;
+        string ans="";
+        s.push_back('*');//so that the last group of characters can be evaluated and pushed into the stack
+        int n=s.length();
+        char temp=s[0];
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(temp==s[i]){
+                
+                count++;
+				//counting adjacent same characters
+                
             }
-            
-            st.push(i);
-            
-            if(continousFreqCount[st.top()] == k){
-                for(int i = 0 ; i < k ; i++){
-                    st.pop();
+            else{
+                if(count%k!=0){//multiples of k are considered to be removed
+			
+                    if(st.empty()==false){
+                        pair<char,int>b=st.top();
+                        if(b.first==temp){	//before inserting the {char,freq} pair into stack check it the char already present in the stack
+						//if present remove it and add its freq to count
+                            st.pop();
+                            count=count+b.second;
+                          
+                        }
+                    }
+                    if(count%k!=0){//multiples of k are considered to be removed
+                    st.push({temp,count%k});}
                 }
+                temp=s[i];//considering the current char that didnt match with the temp as temp 
+                count=1;
             }
             
         }
-        
-        while(!st.empty()){
-            
-            ans += s[st.top()];
+        while(st.empty()==false){
+           pair<char,int>c=st.top();
+          //creating the ans string
+            for(int i=0;i<c.second;i++){
+                ans.push_back(c.first);
+            }
             st.pop();
-            
         }
-        
-        reverse(ans.begin(), ans.end());
+
+        reverse(ans.begin(),ans.end());
         
         return ans;
-        
     }
 };
