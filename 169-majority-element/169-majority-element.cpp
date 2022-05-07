8e86644cb5583d91d31c309d55ad895db7cff6ab
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-05-07 17:18:34 +0530

    Time: 34 ms (22.37%), Space: 19.7 MB (14.39%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        int maxi=INT_MIN;
+        for(auto &it:m)
+        {
+            maxi=max(it.second,maxi);
+            cout<<it.first<<" ";
+        }
+        //auto it=m.find(maxi);
+        int ans;
+        for(auto &it:m)
+        {
+          if(it.second==maxi)
+              ans=it.first;
+        }
+        return ans;
+    }
+};
