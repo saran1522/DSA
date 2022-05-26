a1d1d292e64d09b8fd212770fe3c73ad7c68d86d
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-05-26 23:17:09 +0530

    Time: 6 ms (17.08%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -4,9 +4,9 @@ public:
         int cnt=0;
         while(n>0)
         {
-            if(n%2==1)
-                cnt++;
-            n=n/2;
+            cnt++;
+            n&=(n-1);
+            n/=2;
         }
         return cnt;
     }
