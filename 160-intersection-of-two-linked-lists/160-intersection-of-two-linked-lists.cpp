847c1d73cb89ea631c1d8f72d6bc7d664f197881
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-06-06 21:31:17 +0530

    Time: 102 ms (15.62%), Space: 14.6 MB (31.40%) - LeetHub

---
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int lenA=0,lenB=0,len=0;
+        ListNode* tempA=headA;
+        ListNode* tempB=headB;
+        while(tempA!=NULL)
+        {
+            lenA++;
+            tempA=tempA->next;
+        }
+        while(tempB!=NULL)
+        {
+            lenB++;
+            tempB=tempB->next;
+        }
+        if(lenA>lenB)
+        {
+            len=lenA-lenB;
+            tempA=headA;
+            tempB=headB;
+            while(len--)
+                tempA=tempA->next;
+            while(tempA!=NULL&&tempB!=NULL)
+            {
+                if(tempA==tempB)
+                    return tempA;
+                tempA=tempA->next;
+                tempB=tempB->next;
+            }
+        }
+        else
+        {
+            len=lenB-lenA;
+            tempA=headA;
+            tempB=headB;
+            while(len--)
+                tempB=tempB->next;
+            while(tempA!=NULL&&tempB!=NULL)
+            {
+                if(tempA==tempB)
+                    return tempA;
+                tempA=tempA->next;
+                tempB=tempB->next;
+            }
+        }
+        return NULL;
+    }
+};
