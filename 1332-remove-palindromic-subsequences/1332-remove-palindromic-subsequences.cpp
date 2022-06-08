30cf32cc9a5d4a8a0521a0256b8f765d7775edc5
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-06-08 15:15:23 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     
+        //Note that a subsequence does not necessarily need to be contiguous.
+        //it means we can remove all a in 1 go and b in one go so answer will be 2 
+        //and if string is already palindrom than simply we can remove in 1 pass
         int removePalindromeSub(string s) {
             int i=0,j=s.length()-1;
             while(i<j)
