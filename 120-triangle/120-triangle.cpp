3f01c33175cad9c22197190f67fec22ff632bf8c
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-06-14 00:06:49 +0530

    Time: 14 ms (17.99%), Space: 8.7 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-		int n = triangle.size();
+		/*int n = triangle.size();
 		vector<int>next(n);
 		for(int i = n-1; i >= 0; i--) {
 			vector<int>curr(n);
@@ -16,6 +16,15 @@ public:
 			}
 			next = curr;
 		}
-		return next[0];
+		return next[0];*/
+         int n = triangle.size();
+        vector<int> vec = triangle[n-1];
+        
+        for(int i =n-2;i>=0;i--){
+            for(int j=0;j<triangle[i].size();j++){
+                  vec[j] = triangle[i][j] + min(vec[j],vec[j+1]);        
+            }
+        }
+        return vec[0];
 	}
 };
