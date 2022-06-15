bb87e4399aef4d5f7aac851e0b97f1881fa532e0
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-06-16 00:04:31 +0530

    Time: 12 ms (22.96%), Space: 10.5 MB (12.51%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -14,31 +14,25 @@ public:
         
         if(head==NULL || head->next==NULL)
             return head;
-        
-        //queue<ListNode*>qu;
         ListNode* temp=head;
         head= new ListNode(-1);
         ListNode* tail=head;
         ListNode* newHead=new ListNode(-1);
         ListNode* newTail=newHead;
-        
         while(temp!=NULL)
         {
             if(temp->val<x)
             {
-                tail->next=new ListNode(temp->val);\
+                tail->next=new ListNode(temp->val);
                 tail=tail->next;
             }
-            
             else
             {
                 newTail->next=new ListNode(temp->val);
                 newTail=newTail->next;
             }
-            
             temp=temp->next;
         }
-        
         newTail->next=NULL;
         tail->next=newHead->next;
         head=head->next;
