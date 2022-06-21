07f6d4ff17295c67a20263aea3f7a8be9949857a
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-06-21 23:47:43 +0530

    Time: 681 ms (5.02%), Space: 127.6 MB (5.10%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    //calculate the mid 
-    //check if we can go to the mid building using all bricks and ladders
-    //if we can then check for more building so do lo=mid
-    //else check for less buildings so do hi=mid-1
-    //if check function simply store the difference of buildings from 1 to mid in an array
-    //sort the difference array in decending order so we will use ladders biggest differences of building (i.e. 1 to no of ladders difference )
-    //so start loop from ladders to end of difference array and keep subtracting bricks
-    //if bricks ended then it is not possible to reach on mid building by using all the ladders and bricks so return false
-    //else it is possible so return true
-    
     bool check(vector<int> heights, int bricks, int ladders, int mid){
         //store the height differences which are greater than previous one
 		vector<int> diff;
