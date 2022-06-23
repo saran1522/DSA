5daf973f497edbf449a9d99a3736d98a83a882e6
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-06-23 19:43:06 +0530

    Time: 365 ms (50.00%), Space: 63.4 MB (33.94%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int scheduleCourse(vector<vector<int>>& courses) {
-        if(courses.size() <= 0) return 0;
-        sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b) {
+        if(courses.size() <= 0) 
+            return 0;
+        sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b)
+        {
             return a[1] < b[1];
         });
         priority_queue<int> q;
@@ -10,7 +12,8 @@ public:
         for(auto i : courses) {
             sum += i[0];
             q.push(i[0]);
-            if(sum > i[1]) {
+            if(sum > i[1]) 
+            {
                 sum -= q.top();
                 q.pop();
             }
