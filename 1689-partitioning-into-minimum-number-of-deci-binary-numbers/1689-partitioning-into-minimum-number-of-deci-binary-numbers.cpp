5afb5633db5d20bbfbdd580e9630533ef5ef3824
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-06-27 23:32:43 +0530

    Time: 60 ms (18.64%), Space: 13.5 MB (92.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -3,7 +3,6 @@ public:
     int minPartitions(string n) {
         auto it= max_element(n.begin(),n.end());
         char c=*it;
-        int ans=c-'0';
-        return ans;
+        return c-'0';
     }
 };
