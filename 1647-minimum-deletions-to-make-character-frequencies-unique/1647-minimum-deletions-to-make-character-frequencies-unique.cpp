8b73bf7fc44a6a9b86cf296c9f78215246476c70
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-06-29 00:10:53 +0530

    Time: 46 ms (98.43%), Space: 17.2 MB (81.59%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    //store frequence of every element in an vector 
+    //sort the vector so higest frequency is at righ most and all the same frequencies will come together so that it will be easier in calculations
+    //run a loop from i=24 because 25is the last element which is dont need to be checked
+    //now if any 2 frequencies are same then we make it equal to the previous -1 and add quantnity of removed in delete variable
+    //now that element can be lesser then next element so apply same for that element 
+    //now if any element is 0 means all its left elements are also 0, so break the loop
+    //return delete variable as it as addition of all the deleted element
     int minDeletions(string s) {
         vector<int>freq(26);
         for(char &ch: s)
