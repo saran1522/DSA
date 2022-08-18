4ef937880db73d7874b0daee23bb6ba6b2fc5441
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-08-18 23:58:23 +0530

    Time: 340 ms (52.00%), Space: 78.4 MB (63.73%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        /*cout<<endl;
+        unordered_map<int,int>mp;
+            for(int i=0;i<arr.size();++i)
+               mp[arr[i]]++;
+        set<int>st;
+            for(auto &i:mp)
+                st.insert(i.second);
+        int cnt=0,sum=0;
+        
+        for(auto it=st.rbegin();it!=st.rend();--it)
+        {
+            cout<<*it<<" ";
+            sum+=*it;
+            cnt++;
+            if(sum>=arr.size()/2)
+                return cnt;
+        }
+        return cnt;*/
+        
+        unordered_map<int, int> counter;
+        priority_queue<int> q;
+        int res = 0, removed = 0;
+        
+        for (auto a : arr) counter[a]++;
+        for (auto c : counter) q.push(c.second);
+        
+        while (removed < arr.size() / 2) {
+            removed += q.top();
+            q.pop();
+            res++;
+        }
+        
+        return res;
+    }
+};
