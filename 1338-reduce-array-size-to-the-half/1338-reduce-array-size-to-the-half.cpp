e3ff8344b1b65c8a46162b19bdd0f19a2d2639f0
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-08-19 00:16:33 +0530

    Time: 403 ms (33.95%), Space: 97.7 MB (5.44%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int minSetSize(vector<int>& arr) {
-        /*cout<<endl;
+        //cout<<endl;
         unordered_map<int,int>mp;
             for(int i=0;i<arr.size();++i)
                mp[arr[i]]++;
-        set<int>st;
+        multiset<int>st;
             for(auto &i:mp)
+            {
+                //cout<<i.first<<" "<<i.second<<endl;
                 st.insert(i.second);
+            }
         int cnt=0,sum=0;
         
-        for(auto it=st.rbegin();it!=st.rend();--it)
+        for(auto it=st.rbegin();it!=st.rend();++it)
         {
-            cout<<*it<<" ";
+            //cout<<*it<<",";
             sum+=*it;
+            //cout<<sum<<" ";
             cnt++;
             if(sum>=arr.size()/2)
                 return cnt;
         }
-        return cnt;*/
+        return cnt;
         
-        unordered_map<int, int> counter;
+        /*unordered_map<int, int> counter;
         priority_queue<int> q;
         int res = 0, removed = 0;
         
@@ -33,6 +37,6 @@ public:
             res++;
         }
         
-        return res;
+        return res;*/
     }
 };
