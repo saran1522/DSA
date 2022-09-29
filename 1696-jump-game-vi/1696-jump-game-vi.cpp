51357767377a00897ce75721e6a5d16307cb2fae
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-09-29 23:54:40 +0530

    Time: 960 ms (5.06%), Space: 135.6 MB (12.68%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+   /* int solve(vector<int>&nums,int k,int n,vector<int>dp)
+    {
+        if(n==0)
+            return nums[0];
+        if(dp[n]!=INT_MIN)
+            return dp[n];
+        int maxJump=INT_MIN;
+        for(int i=1;i<=k;++i)
+        {
+            if(n-i>=0)
+            {
+                int jump=solve(nums,k,n-i,dp)+nums[n];
+                dp[n]=max(maxJump,jump);
+                cout<<maxJump;
+            }
+        }
+        return dp[n];
+    }*/
     int maxResult(vector<int>& nums, int k) {
         if(nums.size()==1)return nums[0];
         vector<int>dp(nums.size(),INT_MIN);
@@ -15,5 +33,7 @@ public:
             Q.insert(dp[i]);
         }
         return dp[nums.size()-1];
+        /*vector<int>dp(nums.size(),INT_MIN);
+        return solve(nums,k,nums.size()-1,dp);*/
     }
 };
