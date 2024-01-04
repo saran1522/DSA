eb65d1a20abb698b50b4bce2bb1e6d0852afad6e
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-01-04 18:26:34 +0530

    Time: 109 ms (90.05%), Space: 85.1 MB (90.06%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int>mp;
+        int ans = 0;
+        for(int i=0;i<n;++i)
+            mp[nums[i]]++;
+        for(auto &it:mp)
+        {
+            int freq = it.second;
+            if(freq==1)
+                return -1;
+            if(freq != 4)
+            {
+                ans += freq/3;
+                freq = freq%3;
+            }
+            ans += freq/2;
+            if(freq==1)
+                ans++;
+        }
+        return ans;
+    }
+};
