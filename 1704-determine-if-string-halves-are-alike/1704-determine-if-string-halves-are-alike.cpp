45b640fe9ffe0aefcc3a0625622f88426b0e72a0
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-01-12 23:11:02 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (24.18%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
-        int n=s.size();
-        
-        int a_vowal=0,b_vowal=0;
-        for(int i=0;i<n;++i)
+        int a = 0, b=0;
+        for(int i = 0;i<s.size();++i)
         {
-            if(i<n/2)
-            {
-             if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||
-                s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
-                 a_vowal++;
-            }
-            else
-            {
-             if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||
-                s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
-                 b_vowal++;
-            }
-            
+            if(s[i] == 'a' || s[i]=='e' || s[i]=='i' || s[i] == 'o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i] == 'I' || s[i]=='O' || s[i]=='U'){
+                if(i<s.size()/2) a++;
+                else b++;
+            } 
         }
-        return a_vowal==b_vowal;
+        return a==b;
     }
 };
