8c3c44ab42fe4f81834517101e2a610856c3cd53
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-01-14 16:37:17 +0530

    Time: 62 ms (94.78%), Space: 20.9 MB (89.19%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     bool closeStrings(string s1, string s2) {
+        
         if(s1.size() != s2.size())
             return false;
+        
         vector<int>freq1(26,0);
         vector<int>freq2(26,0);
+        
         for(int i=0; i<s1.size(); ++i)
         {
             freq1[s1[i]-'a']++;
             freq2[s2[i]-'a']++;
         }
-        int sum1 = 0, sum2 =0, ind1 =0, ind2 =0;
+        
         for(int i = 0; i<26; ++i)
         {
-            if(freq1[i]>0)
+            if((freq1[i]>0 && freq2[i]==0) || (freq2[i]>0 && freq1[i]==0))
             {
-                sum1 += freq1[i];
-                ind1 += i;
-            }
-            if(freq2[i]>0)
-            {
-                sum2 += freq2[i];
-                ind2 += i;
+               return false;
             }
         }
+        
         sort(freq1.begin(),freq1.end());
         sort(freq2.begin(),freq2.end());
-        return freq1==freq2 && ind1==ind2;
+        
+        return freq1==freq2;
     }
      
 };
