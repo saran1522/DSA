76c130db425f0e88f523a8771f767b72e1967fad
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-01-31 01:05:46 +0530

    Time: 12 ms (34.44%), Space: 15.4 MB (11.31%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -4,29 +4,20 @@ public:
         stack<int>st;
         for(int i=0;i<tokens.size();++i)
         {
-            // int num = stoi(tokens[i]);
             if(tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/")
             {
-                int first = st.top();
-                st.pop();
-                int second = st.top();
-                st.pop();
+                int first = st.top(); st.pop();
+                int second = st.top(); st.pop();
                 int res;
-                if(tokens[i]=="+")
-                    res = first + second;
-                else if(tokens[i]=="-")
-                    res = second - first;
-                else if(tokens[i]=="*")
-                    res = second * first;
-                else if(tokens[i]=="/")
-                    res = second / first;
+                if(tokens[i]=="+") res = first + second;
+                else if(tokens[i]=="-") res = second - first;
+                else if(tokens[i]=="*") res = second * first;
+                else if(tokens[i]=="/") res = second / first;
                 st.push(res);
             }
                 
-            else{
-                int num = stoi(tokens[i]);
-                st.push(num);
-            }
+            else
+                st.push(stoi(tokens[i]));
         }
         return st.top();
     }
