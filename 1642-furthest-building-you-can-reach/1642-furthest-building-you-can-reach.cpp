d4ee9936950604053dacd6e192b7dff5f6ee09d2
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-02-18 00:28:40 +0530

    Time: 77 ms (64.67%), Space: 57.2 MB (40.68%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
-    //calculate the mid 
-    //check if we can go to the mid building using all bricks and ladders
-    //if we can then check for more building so do lo=mid
-    //else check for less buildings so do hi=mid-1
-    //if check function simply store the difference of buildings from 1 to mid in an array
-    //sort the difference array in decending order so we will use ladders biggest differences of building (i.e. 1 to no of ladders difference )
-    //so start loop from ladders to end of difference array and keep subtracting bricks
-    //if bricks ended then it is not possible to reach on mid building by using all the ladders and bricks so return false
-    //else it is possible so return true
-    
-    bool check(vector<int> heights, int bricks, int ladders, int mid)
-    {
-		vector<int> diff;
-        for(int i = 1; i <= mid; i++)
+    int furthestBuilding(vector<int>& hts, int brks, int lads) {
+        priority_queue<int>pq;
+        for(int i=0; i<hts.size()-1;++i)
         {
-            if(heights[i] > heights[i-1])
-                diff.push_back(heights[i]-heights[i-1]);
+            if(hts[i+1]>hts[i]){
+                int d = hts[i+1]-hts[i];
+                pq.push(-d);
+                if(pq.size()>lads)
+                {
+                    brks += pq.top();
+                    pq.pop();
+                }
+                if(brks<0)
+                    return i;
+            }
         }
-        
-        sort(diff.begin(), diff.end(), greater<int>());
-        int l = diff.size();
-        for(int i = ladders; i < l; i++){
-            if(diff[i] > bricks) 
-                return false;
-            bricks -= diff[i];
-        }
-        return true;
-    }
-    
-    
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        int n = heights.size();
-        int lo = 0, hi = n-1;
-        while(lo < hi)
-        {
-            int mid = lo+((hi-lo)/2)+1;
-            if(check(heights, bricks, ladders, mid))
-                lo = mid;  
-            else
-                hi = mid-1;
-        }
-        return lo;
+        return hts.size()-1;
     }
 };
