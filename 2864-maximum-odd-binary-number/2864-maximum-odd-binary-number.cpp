f152cf06554486e7e3b230c5adf327f099c69ae7
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-03-01 10:27:46 +0530

    Time: 2 ms (80.97%), Space: 8.6 MB (29.81%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
+        // approach 1 
+        // make an ans string; insert all 1s initially in ans
+        // then start inserting 0s in ans till s.size()-1
+        // them swap the last 1 and 0 at the end of the ans string
         string ans = "";
         int j=0;
         for(int i=0;i<s.size() ; ++i){
@@ -17,5 +21,29 @@ public:
             
         swap(ans[lastOne],ans[j-1]);
         return ans;
+        
+        // approach 2
+        // count 0s and 1s in s
+        // insert 1s in ans string till oneCnt -1 (1 for the last position)
+        // insert 0s in ans string till zCnt==0
+        // insert remaining 1 in the ans string
+        //
+        // int oneCnt =0, zCnt=0;
+        // for(char &c:s){
+        //     if(c=='0')
+        //         zCnt++;
+        //     else
+        //         oneCnt++;
+        // }
+        // s="";
+        // while(oneCnt>1){
+        //     s.push_back('1');
+        //     oneCnt--;
+        // }
+        // while(zCnt--){
+        //     s.push_back('0');
+        // }
+        // s.push_back('1');
+        // return s;
     }
 };
