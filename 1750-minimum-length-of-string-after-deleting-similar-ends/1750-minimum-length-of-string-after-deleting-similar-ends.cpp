a66173115ac0100df3b37c165fc5ee2bdf4accfb
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-03-05 21:10:46 +0530

    Time: 23 ms (80.00%), Space: 13.8 MB (70.34%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
+    // start selecting the same characters at both the ends and replace them wiht temp characters (like 'x' & 'y')
+    // also do l++ & r-- as s[l] & s[r] are deleted
+    // if l becomre greater then r that means indexes are intersected so stop
+    // return the r-l + 1
     int minimumLength(string s) {
         int l =0, r=s.size()-1;
         while(l<r){
-            // cout<<l<<" "<<r<<endl;
             char same;
-            if(s[l] == s[r])
+            if(s[l]==s[r])
                 same = s[l];
             else break;
-            while(l<=r &&(s[l]==same || s[r]==same)){
-                if(s[l]==same){
-                    s[l] = 'x';
-                    l++;
-                }
-                    
-                if(s[r]==same){
-                    s[r] = 'y';
-                    r--;
-                }
-                    
+            while(s[l]==same && l<=r){
+                s[l]='x';
+                l++;
             }
+                
+            while(s[r]==same && l<=r){
+                s[r]='y';
+                r--;
+            }
+                
         }
         return r-l+1;
     }
-    
-    //"cab aaaaaaaa abac"
-    // xxxxyyyy
 };
