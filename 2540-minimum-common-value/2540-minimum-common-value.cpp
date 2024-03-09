de416a141d8a76ce6f75d994ec7c9ba8bd07b4cf
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-03-09 11:08:23 +0530

    Time: 67 ms (73.43%), Space: 53.1 MB (29.59%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    // since nums1 & nums2 both are sorted, we can use 2 pointers here
+    // so i for nums1 & j for nums2 (both will start with 0)
+    // run while loop till i & j equals the sizes
+    // the first occurrence of nums1[i] == nums2[j] will be the smallest same element because arrays are sorted and we are traversing from left so return that
+    // if ith is smaller then do i++ (so next greater may be equal to jth)
+    // else do j++ (so next greater may be equal to ith)
+    // if no same element found and loop ends than return -1
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int i=0, j=0;
         while(i<nums1.size() && j<nums2.size()){
