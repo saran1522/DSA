0e31300107ac918d572d58b88e52eb1b83ae7481
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-03-11 12:52:32 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (35.04%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
+    // first store the characters of s & thier frequencies in a hashmap
+    // now all elements in mp has the unique characters in s
+    // now traverse on order string from 0 to size-1
+    // check if map contains the order[i] in it; if true then add it in ans string mp[order[i]] (freq.) no. of times
+    // now we have common characters of s & order in ans string in sorted order
+    // now we have to add characters of s which are not in order
+    // so traverse on hashmap and check if the it.first is present in ans string or not 
+    // if not then add it in ans string in it.second(freq.) no. of times
+    // now ans conains the sorted s according to given order
+    // return ans
     string customSortString(string order, string s) {
         unordered_map<char,int>mp;
         for(int i=0;i<s.size();++i)
             mp[s[i]]++;
         
         string ans = "";
-        int sInd = 0;
         for(int i =0; i<order.size();++i){
             if(mp.find(order[i]) != mp.end()){
                 while(mp[order[i]]--)
