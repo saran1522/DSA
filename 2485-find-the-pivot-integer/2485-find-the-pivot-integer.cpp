898bba148fa271163569655796f38fb809489950
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-03-13 20:59:25 +0530

    Time: 4 ms (35.26%), Space: 7.2 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
+    // calculate total sum by the formula n*(n+1)/2
+    // take m = median of 1 to n 
+    // run a loop till m<n
+    // in each step calculate left sum till m using same formula
+    // calculate right sum by substracting left sum from total sum and adding 'm'
+    // return m if they are equal
+    // return -1 out of the loop
+    // int pivotInteger(int n) {
+    //     if(n==1) return 1;
+    //     int lSum = 0, rSum = 0;
+    //     int tSum = n*(n+1)/2;
+    //     int m = n/2;
+    //     while(m<n){
+    //         int lSum = m*(m+1)/2;
+    //         int rSum = tSum - lSum + m;
+    //         if(lSum == rSum)
+    //             return m;
+    //         else m++;
+    //     }
+    //     return -1;
+    // }
+    
     int pivotInteger(int n) {
-        if(n==1) return 1;
-        int l= 1, r = n, lSum = 0, rSum = n;
-        int tSum = n*(n+1)/2;
-        int m = l+(r-l)/2;
-        while(m<n){
-            int lSum = m*(m+1)/2;
-            int rSum = tSum - lSum + m;
-            if(lSum == rSum)
-                return m;
-            else m++;
-        }
-        return -1;
+        int sum = n * (n + 1) / 2, x = sqrt(sum);
+        return x * x == sum ? x : -1;
     }
 };
