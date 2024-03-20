8f0bd3187e33fc577f653cb4ab03f9c5f2fa3a82
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-03-20 21:09:12 +0530

    Time: 200 ms (7.22%), Space: 98 MB (82.89%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
@@ -10,26 +10,25 @@
  */
 class Solution {
 public:
+    // put a pointer on a and a pointer on b
+    // link a->next to the head of list 2
+    // put a pointer temp = last node of the list2
+    // linke temp->next = b
+    // so lists will be merged as desired
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
         ListNode* start = list1, *end = list1, *temp = list2;
-        // ListNode* end = list2;
         a -= 1;
         b += 1;
-        while(a--)
-            start = start->next;
         
-        // cout<<start->val<<endl;
-        while(b--)
-            end = end->next;
-        // cout<<end->val<<endl;
-        while(temp->next != NULL)
-            temp = temp->next;
-        // cout<<temp->val<<endl;
+        while(a--) start = start->next;
+        
+        while(b--) end = end->next;
+        
+        while(temp->next != NULL) temp = temp->next;
         
         start->next = list2;
         temp->next = end;
         
         return list1;
-        
     }
 };
