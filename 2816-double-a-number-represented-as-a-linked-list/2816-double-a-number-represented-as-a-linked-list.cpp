c4860e19d880c5d2414f0f9f10d2eecbc68a5a02
Saran Sinha <90507409+saran1522@users.noreply.github.com>
2024-05-07 20:59:50 +0530

    Time: 178 ms (52.99%), Space: 120.8 MB (65.02%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -11,32 +11,21 @@
 class Solution {
 public:
     ListNode* doubleIt(ListNode* head) {
-        vector<int>nodes;
+        
+        if(head->val >4)
+            head = new ListNode(0, head);
+        
         ListNode* temp = head;
+        
         while(temp != NULL){
-            nodes.push_back(temp->val);
-            temp = temp->next;
-        }
-        
-        int carry =0;
-        for(int i=nodes.size()-1;i>=0;--i){
-            int res = nodes[i]*2 + carry;
-            nodes[i] = res%10;
-            carry = res/10;
-        }
-        
-        ListNode* newHead = new ListNode(nodes[0]);
-        temp = newHead;
+            temp->val = (2* temp->val)%10;
             
-        for(int i=1;i<nodes.size();++i){
-            temp->next = new ListNode(nodes[i]);
+            if(temp->next && temp->next->val >4)
+                temp->val++;
+            
             temp = temp->next;
         }
-        if(carry != 0){
-            ListNode* newNode = new ListNode(carry);
-            newNode->next = newHead;
-            newHead = newNode;
-        }
-            return newHead;
+        
+        return head;
     }
 };
